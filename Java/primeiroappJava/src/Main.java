ff7ae795ef21149fb9de818b3845a4eae73485a1
ovictorferreira <ferreirasvictorj@gmail.com>
2025-08-11 15:03:22 -0300

    Initial commit

---
+++ Java/primeiroappJava/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Criando uma variável
+        var nome = "João";
+
+        //Fazendo output do dado (print)
+        System.out.println("Olá" + nome);
+
+        //Sem o ln ele não pula a linha
+        var sobrenome = "Ferreira";
+        System.out.print(nome);
+        System.out.print(sobrenome);
+
+        //Console é um meio de comunicação entre o programador e o sistema
+
+        //Código amarelo = Anotações
+        //Podemos colocar um dado dentro de outro dado com as anotações, facilitando na hora de colocar o codigo
+        //https://start.spring.io/ - usaremos muito para criar projetos
+
+        }
+    }
---
+++ Java/variaveis/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TRABALHANDO COM VARIAVEIS
+        // repositório = local na memoria com conteudo e endereço
+        // Criar um espaço an memoria para armazenar um dado
+
+        var nome = "João victor";
+
+    }
+}
