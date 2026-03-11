7c01a701c5162a7d6f4fc007d6a05d649d586e0a
ovictorferreira <ferreirasvictorj@gmail.com>
2026-03-11 20:32:51 -0300

    Aula 25 - 04/03/2026 - Continuação bankapi

--- Java/bankapi/src/main/java/com/curso/bankapi/BankapiApplication.java
+++ Java/bankapi/src/main/java/com/curso/bankapi/BankapiApplication.java
@@ -1,5 +1,9 @@
 package com.curso.bankapi;
 
+import com.curso.bankapi.services.AccountService;
+import com.curso.bankapi.services.CustomerService;
+import com.curso.bankapi.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +14,22 @@ public class BankapiApplication {
 		SpringApplication.run(BankapiApplication.class, args);
 	}
 
+    //-------------------- Zona de Teste ------------------------
+    // Done: Injetar serviços -- Application esta fora do MVC então nao funciona o @Autowired, tem q fazer por contructor
+    private CustomerService customerService;
+    private AccountService accountService;
+    private TransactionService transactionService;
+
+    public BankapiApplication(TransactionService transactionService, AccountService accountService, CustomerService customerService) {
+        this.transactionService = transactionService;
+        this.accountService = accountService;
+        this.customerService = customerService;
+    }
+
+    // Todo: testar serviços de Customer
+
+    // Todo: Testar serviços de Account
+
+    // Todo: Testar serviços de transactions
+    //-------------------- Zona de Teste ------------------------
 }
