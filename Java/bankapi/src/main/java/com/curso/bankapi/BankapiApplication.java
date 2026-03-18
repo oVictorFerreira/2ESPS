5caad45621e8e164141b07fa27482802510cad8b
ovictorferreira <ferreirasvictorj@gmail.com>
2026-03-18 18:24:03 -0300

    Aula 27 - 18/03/2026 - Continuação bankapi

--- Java/bankapi/src/main/java/com/curso/bankapi/BankapiApplication.java
+++ Java/bankapi/src/main/java/com/curso/bankapi/BankapiApplication.java
@@ -1,35 +1,63 @@
 package com.curso.bankapi;
 
+import com.curso.bankapi.models.Customer;
 import com.curso.bankapi.services.AccountService;
 import com.curso.bankapi.services.CustomerService;
 import com.curso.bankapi.services.TransactionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class BankapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BankapiApplication.class, args);
 
-    //-------------------- Zona de Teste ------------------------
-    // Done: Injetar serviços -- Application esta fora do MVC então nao funciona o @Autowired, tem q fazer por contructor
-    private CustomerService customerService;
-    private AccountService accountService;
-    private TransactionService transactionService;
-
-    public BankapiApplication(TransactionService transactionService, AccountService accountService, CustomerService customerService) {
-        this.transactionService = transactionService;
-        this.accountService = accountService;
-        this.customerService = customerService;
+        testador();
     }
 
-    // Todo: testar serviços de Customer
+    //ZONA DE TESTE ++++++++++++++++++++++++++++++++++++++++++++
+    //DONE: Injetar Serviços - Application está fora do MVC!!! Injetar por construtores
+    private static CustomerService customerService;
+    private static AccountService accountService;
+    private static TransactionService transactionService;
 
-    // Todo: Testar serviços de Account
+    public BankapiApplication(CustomerService customerService, AccountService accountService, TransactionService transactionService) {
+        this.customerService = customerService;
+        this.accountService = accountService;
+        this.transactionService = transactionService;
+    }
 
-    // Todo: Testar serviços de transactions
-    //-------------------- Zona de Teste ------------------------
+    static void testador(){
+
+        //DONE: Testar servicos de Customer
+        //Criando um customer
+        Customer customerA = new Customer("Maria Francisca", "111.111.111-11");
+        //customerService.createCustomer(customerA);
+
+        //Buscando um customer por id
+        //customerService.customerById(3);
+
+        //Listando todos os customers
+        //customerService.allCustomers();
+
+        //TODO: Testar serviços de Account
+        //Criando uma conta
+        //accountService.createAccount(2);
+
+        //Buscando uma conta por num de conta
+        //accountService.accountByAcNumber(UUID.fromString("ef19d3fd-cd22-4b19-80f6-5bfc24836b02"));
+
+        //Listando todas as contas
+        //accountService.allAccounts();
+
+        //TODO: Testar Serviços de Transactions
+        //transactionService.deposit(UUID.fromString("ef19d3fd-cd22-4b19-80f6-5bfc24836b02"), 1000.0f);
+        //transactionService.withdraw(UUID.fromString("ef19d3fd-cd22-4b19-80f6-5bfc24836b02"), 250.0f);
+        //transactionService.listByAccount(UUID.fromString("ef19d3fd-cd22-4b19-80f6-5bfc24836b02"));
+    }
+
+    //ZONA DE TESTE ++++++++++++++++++++++++++++++++++++++++++++
 }
---
+++ Java/bankapi/src/main/java/com/curso/bankapi/controllers/json/CustomerJSONController.java
@@ -0,0 +1,21 @@
+package com.curso.bankapi.controllers.json;
+
+
+// Todo: Indicação dos annotations necessarios
+public class CustomerJSONController {
+
+
+    // Todo: Injeção de DPS
+
+    // Todo: Metod0 GET - Listar todos os customers
+    //Endpoint: http://localhost:8080/json/customers
+
+    // Todo: Métod0 GET - Buscar customer por ID
+    //Endpoint: http://localhost:8080/json/customers/customerId
+
+
+    // Todo: Métod0 POST - Criar novo Customer
+    // Endpoint: http://localhost:8080/json/customers/create
+    // Body: {"name":"Joaquim José", "cpf":"000.000.000-00"}
+
+}
