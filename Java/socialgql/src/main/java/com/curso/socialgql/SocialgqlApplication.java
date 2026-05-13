42c736d9e6597801b68a65e920ccb05b3f5728e9
ovictorferreira <ferreirasvictorj@gmail.com>
2026-05-13 20:32:33 -0300

    Aula GraphQL

---
+++ Java/socialgql/src/main/java/com/curso/socialgql/SocialgqlApplication.java
@@ -0,0 +1,13 @@
+package com.curso.socialgql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialgqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialgqlApplication.class, args);
+	}
+
+}
---
+++ Java/socialgql/src/test/java/com/curso/socialgql/SocialgqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.socialgql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialgqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
